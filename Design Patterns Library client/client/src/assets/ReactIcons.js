72cd6417034b2386838b0aa8b4be0d2ae22da795
GeorgeDoLee <Gio.dolidze1234@gmail.com>
2024-11-27 11:43:09 +0400

    separate file for all the react icons for easier accces

---
+++ Design Patterns Library client/client/src/assets/ReactIcons.js
@@ -0,0 +1,29 @@
+import { IoConstructSharp, IoExtensionPuzzleOutline, IoBookOutline } from "react-icons/io5";
+import { PiTreeStructure } from "react-icons/pi";
+import { BsChatRightDots } from "react-icons/bs";
+import { FaRegLightbulb, FaCode } from "react-icons/fa";
+import { FaListCheck } from 'react-icons/fa6';
+import { RiFlowChart } from "react-icons/ri";
+import { LiaProjectDiagramSolid } from "react-icons/lia";
+import { MdCompareArrows } from "react-icons/md";
+import { MdContentCopy } from "react-icons/md";
+import { FaSortAlphaDown, FaSortAlphaUp } from "react-icons/fa";
+import { IoSearchOutline } from "react-icons/io5";
+
+export {
+    IoConstructSharp,
+    IoExtensionPuzzleOutline,
+    IoBookOutline,
+    PiTreeStructure,
+    BsChatRightDots,
+    FaRegLightbulb,
+    FaCode,
+    FaListCheck,
+    RiFlowChart,
+    LiaProjectDiagramSolid,
+    MdCompareArrows,
+    MdContentCopy,
+    FaSortAlphaDown,
+    FaSortAlphaUp,
+    IoSearchOutline
+}
