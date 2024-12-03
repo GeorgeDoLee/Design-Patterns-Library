de429b216d934468d10ef881400512c14d0c9e9b
GeorgeDoLee <Gio.dolidze1234@gmail.com>
2024-12-03 18:18:24 +0400

    use static files

--- Design Patterns Library client/client/src/pages/ClassificationGuidePage.js
+++ Design Patterns Library client/client/src/pages/ClassificationGuidePage.js
@@ -3,33 +3,52 @@ import MainLayout from '../components/Main Layout/MainLayout'
 import useFetch from '../hooks/useFetch'
 import DownloadBook from '../components/DownloadBook';
 
+import { useState } from 'react';
+
 const Classification = ({ classification }) => {
+  const [isDescriptionVisible, setIsDescriptionVisible] = useState(true);
   const descriptionLines = classification.description.split('\n');
+
+  const toggleDescriptionVisibility = () => {
+    setIsDescriptionVisible(!isDescriptionVisible);
+  };
 
   return (
     <div>
       <div className="flex items-center gap-3 py-2 border-b border-opacity-50 md:gap-5 border-primaryText">
-        <img 
-          src={`${process.env.REACT_APP_API_BASE_URL}/${classification.icon}`} 
-          alt={classification.name} 
-          className='w-auto h-8'
+        <img
+          src={`${process.env.REACT_APP_API_BASE_URL}/${classification.icon}`}
+          alt={classification.name}
+          className="w-auto h-8"
         />
         <h1 className="text-lg font-semibold md:text-xl">
           {classification.name}
         </h1>
+        <button
+          onClick={toggleDescriptionVisibility}
+          className="ml-auto text-gray-600 hover:text-gray-800"
+          aria-label={isDescriptionVisible ? "Hide description" : "Show description"}
+        >
+          {isDescriptionVisible ? (
+            <span>🔽</span> // Down arrow icon (visible)
+          ) : (
+            <span>▶️</span> // Right arrow icon (hidden)
+          )}
+        </button>
       </div>
-      <p className="mt-2 text-base text-primaryText">
-        {descriptionLines.map((line, index) => (
-          <span key={index}>
-            {line}
-            <br />
-          </span>
-        ))}
-      </p>
+      {isDescriptionVisible && (
+        <p className="mt-2 text-base text-primaryText">
+          {descriptionLines.map((line, index) => (
+            <span key={index}>
+              {line}
+              <br />
+            </span>
+          ))}
+        </p>
+      )}
     </div>
   );
 };
-
 
 const ClassificationGuidePage = () => {
   const {data: classifications, isLoading, error} = useFetch('/Classification');
--- Design Patterns Library server/Design Patterns Library/Program.cs
+++ Design Patterns Library server/Design Patterns Library/Program.cs
@@ -38,6 +38,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors("AllowAllOrigins");
 
+app.UseStaticFiles();
+
 app.UseHttpsRedirection();
 
 app.MapControllers();
